a1ad598bbf6a300616dde496bd54e4b04408a296
vlas20121 <vlas20121@gmail.com>
2023-12-01 13:55:12 +0300

    add opencv/*

---
+++ opencv/d.bat
@@ -0,0 +1,7 @@
+md build
+cd build
+conan install .. -of . --build=missing > 1
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake  > 2
+cmake --build . --config Release  > 3
+Release\test_package.exe
+start atom_image.jpg
